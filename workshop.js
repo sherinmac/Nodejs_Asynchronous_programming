f1c56a37f700c5839cd3ecb4bde8748b559e2e28
Sherin A G <sherin.ag@citrusinformatics.com>
2019-07-24 16:20:14 +0530

    Promises with out  Async and wait

--- workshop.js
+++ workshop.js
@@ -1,12 +1,37 @@
 var timeConsumingFunction = (params) => {
 
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
 
         try {
             setTimeout(() => {
-                console.log("Timer fun triggered");
-                resolve("success");
+            //     console.log("Timer fun triggered");
+            //     resolve("success");
+
+            var N = 100;
+            var foo = Array.apply(null, { length: N }).map(Number.call, Number)
+
+            // foo.forEach(e => {
+            //     console.log(e);
+
+            // });
+
+            for (let index = 0; index < foo.length; index++) {
+                const element = foo[index];
+                console.log(element);
+                //timeConsumingFunction();
+            }
+
+           
+
+
+
+
             }, 1000);
+
+           
+            resolve("success");
+
+            
 
         } catch (error) {
             reject(error);
@@ -16,7 +41,18 @@ var timeConsumingFunction = (params) => {
 
 };
 
-(async () => {
-    await timeConsumingFunction();
+(() => {
+    // var result =  await timeConsumingFunction();
+    // console.log("After function call", result);
+
+    timeConsumingFunction()
+        .then(result => {
+            console.log(result);
+
+        })
+        .catch(err => {
+            console.error(result);
+
+        });
     console.log("After function call");
 })();
